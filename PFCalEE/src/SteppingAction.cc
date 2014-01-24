5697ac4f97fd23b0a88a421fd550dd6fe3afa68c
amagnan <a.magnan@imperial.ac.uk>
2014-01-24 11:48:11 +0100

    retrieve hit position from G4Step

--- PFCalEE/src/SteppingAction.cc
+++ PFCalEE/src/SteppingAction.cc
@@ -20,11 +20,26 @@ SteppingAction::~SteppingAction()
 //
 void SteppingAction::UserSteppingAction(const G4Step* aStep)
 {
-  G4VPhysicalVolume* volume = aStep->GetPreStepPoint()->GetTouchableHandle()->GetVolume();
+  // get PreStepPoint
+  const G4StepPoint *thePreStepPoint = aStep->GetPreStepPoint();
+  // get TouchableHandle
+  G4TouchableHandle theTouchable = thePreStepPoint->GetTouchableHandle();
+
+
+  G4VPhysicalVolume* volume = theTouchable->GetVolume();
   G4double edep = aStep->GetTotalEnergyDeposit();
   G4double stepl = 0.;
   if (aStep->GetTrack()->GetDefinition()->GetPDGCharge() != 0.) stepl = aStep->GetStepLength();
   G4int pdgId=aStep->GetTrack()->GetDefinition()->GetPDGEncoding(); 
   G4double globalTime=aStep->GetTrack()->GetGlobalTime();
-  eventAction_->Detect(edep,stepl,globalTime,pdgId,volume);
+
+  G4ThreeVector position = thePreStepPoint->GetPosition();
+  // get local hit position using touchable with theGlobalPos
+  //G4ThreeVector LocalHitPos = theTouchable->GetHistory()->GetTopTransform().TransformPoint(position);
+ 
+  //G4cout << "Pre   position " << volume->GetName() << " " << position << G4endl;
+  //G4cout << "Post  position " << aStep->GetPostStepPoint()->GetTouchableHandle()->GetVolume()->GetName() << " " << aStep->GetPostStepPoint()->GetPosition() << G4endl;
+  //G4cout << "Local position " << LocalHitPos << G4endl;
+  
+  eventAction_->Detect(edep,stepl,globalTime,pdgId,volume,position);
 }
