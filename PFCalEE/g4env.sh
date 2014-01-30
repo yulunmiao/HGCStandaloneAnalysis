d771d14b62b931915016b0210559337b8c5ab092
amagnan <a.magnan@imperial.ac.uk>
2014-01-30 09:32:25 +0100

    add bits to compile with HepMC

--- PFCalEE/g4env.sh
+++ PFCalEE/g4env.sh
@@ -2,7 +2,8 @@ export USERBASE=`pwd`
 source /afs/cern.ch/sw/lcg/contrib/gcc/4.6/x86_64-slc6-gcc46-opt/setup.sh 
 export QTHOME=/afs/cern.ch/sw/lcg/external/qt/4.8.4/x86_64-slc6-gcc46-opt/
 export G4BASE=/afs/cern.ch/sw/lcg/external/geant4
+export HEPMC_DIR=/afs/cern.ch/sw/lcg/external/HepMC/2.06.08/x86_64-slc6-gcc47-opt/
 source $G4BASE/9.6.p02/x86_64-slc6-gcc46-opt/share/Geant4-9.6.2/geant4make/geant4make.sh
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$XERCESCROOT/lib:$USERBASE/userlib/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$XERCESCROOT/lib:$HEPMC_DIR/lib:$USERBASE/userlib/lib:$USERBASE/analysis/lib
 source /afs/cern.ch/sw/lcg/contrib/gcc/4.6/x86_64-slc6/setup.sh
 source /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.09/x86_64-slc6-gcc46-opt/root/bin/thisroot.sh
