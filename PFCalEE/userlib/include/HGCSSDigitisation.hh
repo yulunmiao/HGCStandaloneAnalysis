5b0020bcf516f8a53310b0f915f31c3e59f6c1f4
amagnan <a.magnan@imperial.ac.uk>
2014-07-11 16:33:26 +0200

    correct digi default param

--- PFCalEE/userlib/include/HGCSSDigitisation.hh
+++ PFCalEE/userlib/include/HGCSSDigitisation.hh
@@ -35,9 +35,9 @@ public:
     mipToADC_[DetectorEnum::FECAL] = 4;//ADC per mips.
     mipToADC_[DetectorEnum::MECAL] = 4;
     mipToADC_[DetectorEnum::BECAL] = 4;
-    mipToADC_[DetectorEnum::FHCAL] = 50;
-    mipToADC_[DetectorEnum::BHCAL1] = 50;
-    mipToADC_[DetectorEnum::BHCAL2] = 50;
+    mipToADC_[DetectorEnum::FHCAL] = 4;
+    mipToADC_[DetectorEnum::BHCAL1] = 4;
+    mipToADC_[DetectorEnum::BHCAL2] = 4;
     timeCut_[DetectorEnum::FECAL] = 25;//ns
     timeCut_[DetectorEnum::MECAL] = 25;//ns
     timeCut_[DetectorEnum::BECAL] = 25;//ns
