118b4b47d3ed2d94f94b3e4ac7dbf1ebf1ec531c
amagnan <a.magnan@imperial.ac.uk>
2014-07-18 17:35:43 +0200

    global compensation

--- PFCalEE/analysis/runAllFill.sh
+++ PFCalEE/analysis/runAllFill.sh
@@ -16,7 +16,7 @@ do
 	    for e in 5 10 15 20 25 30 40 50 60 80 100 150 200 300 400 500
 	    do
 		echo "Processing v${v}_p${p}_e${e}_run${r}"
-		./bin/validation 0 $INPATH/$p/ HGcal_version${v}_model3_BOFF_e${e}.root Digi_version${v}_model3_BOFF_e${e}.root $MYDIR/validation_e${e}.root 3 >& v${v}_p${p}_e${e}.log &
+		./bin/globalCompensation 0 $INPATH/$p/ HGcal_version${v}_model3_BOFF_e${e}.root Digi_version${v}_model3_BOFF_e${e}.root $MYDIR/globalCompensation_e${e}.root 3 >& gc_v${v}_p${p}_e${e}.log &
 	    done
 	done
     done
