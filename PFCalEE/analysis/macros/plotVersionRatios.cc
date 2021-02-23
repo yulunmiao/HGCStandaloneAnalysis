342a6f20e9b764239007fa80bf05d67aac6d6e72
Bruno Alves <balves16@hotmail.com>
2021-02-23 23:28:53 +0100

    beautify version ratio plot for Chris

--- PFCalEE/analysis/macros/plotVersionRatios.cc
+++ PFCalEE/analysis/macros/plotVersionRatios.cc
@@ -8,6 +8,7 @@
 #include "TStyle.h"
 #include "TPad.h"
 #include "TFile.h"
+#include "TF1.h"
 #include "TH1F.h"
 #include "TLine.h"
 #include "TLegend.h"
@@ -91,7 +92,7 @@ int plotVersionRatios(const InputParserPlotEGResoRatios& ip) {
   }
 
   std::unordered_map<std::string, std::string> vmap;
-  vmap["60"] = "TDR (no neutron moderator)";
+  vmap["60"] = "TDR";
   vmap["70"] = "Scenario 13";
 
   for (unsigned ieta(0); ieta<etas_s; ++ieta)
@@ -136,52 +137,36 @@ int plotVersionRatios(const InputParserPlotEGResoRatios& ip) {
       assert(x1v == x2v);
       assert(x1v.size() == y1v.size());
       
-      double xbins[x1v.size()+1];
-      for(unsigned q(0); q<x1v.size(); ++q) {
-	if(q==0)
-	  xbins[q] = x1v[q]-5.;
-	else
-	  xbins[q] = (x1v[q-1]+x1v[q])/2.;
-      }
-      xbins[x1v.size()] = x1v.back()+5.;
-
       std::string name = "cRatio" + std::to_string(ieta);
       std::string title = "resoRatio" + std::to_string(static_cast<int>(etas[ieta]*10.f));
-      TCanvas *c = new TCanvas(name.c_str(), title.c_str(), 1000, 600);
-      c->SetRightMargin(0.13);
-      c->SetLeftMargin(0.05);
-      c->SetBottomMargin(0.50);
+      TCanvas *c = new TCanvas(name.c_str(), title.c_str(), 800, 600);
+      c->SetRightMargin(0.1);
+      c->SetLeftMargin(0.1);
+      c->SetBottomMargin(-0.3);
       c->Draw();
-      TPad *p1 = new TPad("p1","p1", 0.1, 0.33, 0.9, 1.);
+      TPad *p1 = new TPad("p1","p1", 0.01, 0.45, 0.99, 1.);
       p1->SetBottomMargin(0.);
       p1->Draw();
       p1->cd();
 
-      std::string hname = "h" + std::to_string(static_cast<int>(etas[ieta]*10.)) + "_" + ip.tag();
-      std::string htitle = "hReso" + std::to_string(static_cast<int>(etas[ieta]*10.)) + "_" + ip.tag();;
-      TH1F *h1 = new TH1F(hname.c_str(), htitle.c_str(), x1v.size(), xbins);
-      TH1F *h2 = new TH1F(hname.c_str(), htitle.c_str(), x1v.size(), xbins);
-      for(unsigned q(0); q<x1v.size(); ++q) {
-	h1->SetBinContent(q+1,y1v[q]);
-	h2->SetBinContent(q+1,y2v[q]);
-	h1->SetBinError(q+1,e1v[q]);
-	h2->SetBinError(q+1,e2v[q]);
-      }
-      h1->SetTitle("");
-      h2->SetTitle("");
-      h1->SetMarkerStyle(1);
-      h1->SetMarkerColor(1);
-      h1->SetLineColor(1);
-      //h1->GetXaxis()->SetRangeUser(-5.,295.);
-      //h1->GetYaxis()->SetRangeUser(0.001,0.16);
-      h1->GetXaxis()->SetLabelSize(0.05);
-      h1->GetYaxis()->SetLabelSize(0.05);
-      h1->GetYaxis()->SetTitleSize(0.05);
-      h1->GetYaxis()->SetTitle("#sigma/E");
-      h1->SetLineColor(kRed);
-      h1->Draw();
-      h2->SetLineColor(kBlue);
-      h2->Draw("SAME");
+      resoV[idx1]->SetTitle("");
+      resoV[idx2]->SetTitle("");
+      resoV[idx1]->GetYaxis()->SetRangeUser(0.,0.09);
+      resoV[idx1]->GetXaxis()->SetLabelSize(0.00);
+      resoV[idx1]->GetYaxis()->SetLabelSize(0.06);
+      resoV[idx1]->GetYaxis()->SetTitle("#sigma/E");
+      resoV[idx1]->GetYaxis()->SetTitleSize(0.07);
+      resoV[idx1]->GetYaxis()->SetTitleOffset(0.65);
+      resoV[idx1]->SetMarkerStyle(20);
+      resoV[idx1]->SetMarkerSize(1.5);
+      resoV[idx1]->SetMarkerColor(kRed);
+      resoV[idx1]->GetFunction("resoRaw")->SetLineColor(kRed);
+      resoV[idx1]->Draw("APE");
+      resoV[idx2]->SetMarkerStyle(22);
+      resoV[idx2]->SetMarkerSize(1.5);
+      resoV[idx2]->SetMarkerColor(kBlue);
+      resoV[idx2]->GetFunction("resoRaw")->SetLineColor(kBlue);
+      resoV[idx2]->Draw("PE SAME");
 
       std::stringstream etavalstr;
       etavalstr << std::fixed << std::setprecision(1) << etas[ieta];
@@ -190,53 +175,73 @@ int plotVersionRatios(const InputParserPlotEGResoRatios& ip) {
       sprintf(buf1,"#gamma, PU 0");
       TLatex lat1;
       lat1.SetTextSize(0.06);
-      lat1.DrawLatexNDC(0.20,0.8,buf1);
+      lat1.DrawLatexNDC(0.13,0.84,buf1);
       sprintf(buf1,"r = %3.0f mm", radius_map[ip.signalRegions()[0]]);
-      lat1.DrawLatexNDC(0.20,0.74,buf1);
+      lat1.DrawLatexNDC(0.13,0.77,buf1);
       sprintf(buf1,("|#eta| = " + etavalstr.str()).c_str());
-      lat1.DrawLatexNDC(0.20,0.69,buf1);
-
+      lat1.DrawLatexNDC(0.13,0.71,buf1);
+      lat1.SetTextSize(0.07);
       lat1.DrawLatexNDC(0.11,0.92,"HGCAL G4 standalone");
       
-      auto legend = new TLegend(0.5,0.7,0.9,0.9);
-      legend->SetTextSize(0.05);
-      legend->AddEntry(h1,vmap[versions[0]].c_str(),"f");
-      legend->AddEntry(h2,vmap[versions[1]].c_str(),"f");
-      legend->Draw();
-
       c->cd(0);
-      TPad *p2 = new TPad("p2","p2",0.1,0.,0.9,0.33);
+      TPad *p2 = new TPad("p2","p2",0.01,0.1,0.99,0.45);
       p2->SetTopMargin(0.0);
       p2->Draw();
       p2->cd();
 
-      std::string hdivname = "hdiv" + std::to_string(static_cast<int>(etas[ieta]*10.)) + "_" + ip.tag();
-      std::string hdivtitle = "hResodiv" + std::to_string(static_cast<int>(etas[ieta]*10.)) + "_" + ip.tag();
-      TH1F *div = new TH1F(hdivname.c_str(), hdivtitle.c_str(), x1v.size(), xbins);
-      div->Sumw2();
-      div->Divide(h1,h2,1.,1.,"b");
-      div->SetAxisRange(0.8, 1.1,"Y");
+      float xvals[x1v.size()], yvals[y1v.size()];
+      float exvals[x1v.size()], eyvals[y1v.size()];
+      assert(y1v.size() == y2v.size());
+      assert(y1v.size() == x1v.size());
+      for(unsigned i(0); i<x1v.size(); ++i) {
+	xvals[i] = x1v[i];
+	exvals[i] = 0;
+	yvals[i] = y1v[i] / y2v[i];
+	//error propagation of division
+	eyvals[i] = (y1v[i]*y1v[i])/(y2v[i]*y2v[i]) * std::sqrt( (e1v[i]*e1v[i])/(y1v[i]*y1v[i]) + (e2v[i]*e2v[i])/(y2v[i]*y2v[i]) );
+      }
+      
+      TGraphErrors *div = new TGraphErrors(x1v.size(), xvals, yvals, exvals, eyvals);
       div->SetTitle("");
-      div->GetXaxis()->SetTitle("E (GeV)");
-      div->GetXaxis()->SetTitleSize(0.13);
-      div->GetXaxis()->SetLabelSize(0.13);
-      div->GetYaxis()->SetLabelSize(0.1);
-      div->Draw();
-      TLine *line = new TLine(x1v[0],1,x1v.back(),1);
+      div->GetXaxis()->SetTitle("E [GeV] ");
+      div->GetXaxis()->SetTitleSize(0.11);
+      div->GetXaxis()->SetLabelSize(0.1);
+      div->GetXaxis()->SetTitleOffset(-0.4);
+      div->GetYaxis()->SetLabelSize(0.08);
+      div->SetMarkerColor(kGreen);
+      div->SetMarkerStyle(20);
+      div->SetMarkerSize(1.3);
+      div->Draw("ap");
+
+      float bol = div->GetXaxis()->GetBinLowEdge(div->GetXaxis()->GetFirst());
+      float eol = div->GetXaxis()->GetBinUpEdge(div->GetXaxis()->GetLast());
+
+      TLine *line = new TLine(bol,1,eol,1);
       line->SetLineStyle(2);
       line->Draw("same");
 
       double factor = TMath::Sqrt(26./28.);
-      TLine *line2 = new TLine(x1v[0],factor,x1v.back(),factor);
+      TLine *line2 = new TLine(bol,factor,eol,factor);
       line2->SetLineColor(kMagenta);
       line2->SetLineStyle(5);
       line2->Draw("same");
+
+      p1->cd();
+      auto legend = new TLegend(0.6,0.55,0.9,0.9);
+      legend->SetTextSize(0.07);
+      legend->AddEntry(resoV[idx1],vmap[versions[0]].c_str(),"lep");
+      legend->AddEntry(resoV[idx2],vmap[versions[1]].c_str(),"lep");
+      std::string ratiostr = vmap[versions[0]] + " / " + vmap[versions[1]];
+      legend->AddEntry(div,ratiostr.c_str(),"lep");
+      legend->AddEntry(line2,"sqrt(26/28)","l");
+      legend->Draw();
 
       foutReso->cd();
       div->Write();
       foutReso->Write();
       
       c->SaveAs((dirInBase2 + title + ".png").c_str());
+      c->SaveAs((dirInBase2 + title + ".pdf").c_str());
     }
 
   return 0;
